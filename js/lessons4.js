de4e4650c960ce67feba9042c79f22abf2f8f13e
noname068218 <128069790+noname068218@users.noreply.github.com>
2023-08-26 11:21:05 +0200

    dd

--- js/lessons4.js
+++ js/lessons4.js
@@ -1,82 +1,89 @@
-// ЗАДАЧА 1
-// Створіть карточки товару з масиву використовуючи createElement
+// // ЗАДАЧА 1
+// // Створіть карточки товару з масиву використовуючи createElement
 
-// js/
+// // js/
 // export default [
-//     {
-//       name: 'pajero',
-//       img: "https://smart-style.com.ua/uploads/mitsubishi-pajero-2011.jpg",
-//       price: 950000,
-//       description: "Широкий вибір комплектацій дозволяє підібрати автомобіль під ваші потреби та стиль життя. Тип привода, потужність двигуна та кількість місць у салоні — порівняти комплектації та ціни стало ще простіше.",
-//     },
-
-//{
-//     name: 'jeep',
+//   {
+//     name: "pajero",
+//     img: "https://smart-style.com.ua/uploads/mitsubishi-pajero-2011.jpg",
+//     price: 950000,
+//     description:
+//       "Широкий вибір комплектацій дозволяє підібрати автомобіль під ваші потреби та стиль життя. Тип привода, потужність двигуна та кількість місць у салоні — порівняти комплектації та ціни стало ще простіше.",
+//   },
+//   {
+//     name: "jeep",
 //     img: "https://ukravto.ua/files/Text/190225_JEEP_NEW_CHEROKEE_9.jpg",
 //     price: 1200000,
 //     description:
-//       'Як і всі моделі Jeep - володіє безпрецедентним поєднанням ходових якостей, комфортності та міського шику. 2,4-літровий бензиновий двигун в поєднанні з шестиступінчастою автоматичною коробкою передач, яка відрізняється плавністю перемикання.',
-    
+//       "Як і всі моделі Jeep - володіє безпрецедентним поєднанням ходових якостей, комфортності та міського шику. 2,4-літровий бензиновий двигун в поєднанні з шестиступінчастою автоматичною коробкою передач, яка відрізняється плавністю перемикання.",
 //   },
 //   {
-//     name: 'toyota',
+//     name: "toyota",
 //     img: "https://molbuk.ua/uploads/posts/2021-06/1623319390_toyota_camry.jpg",
 //     price: 999000,
 //     description:
-//       'Оснащений тихим самозарядним гібридним двигуном об’ємом 2,5 літра, автомобіль забезпечує значний крутний момент на низьких оборотах і феноменальну потужність на високих, а також краще реагування на дії водія та легше прискорення.',
+//       "Оснащений тихим самозарядним гібридним двигуном об’ємом 2,5 літра, автомобіль забезпечує значний крутний момент на низьких оборотах і феноменальну потужність на високих, а також краще реагування на дії водія та легше прискорення.",
 //   },
-// {
-//     name: 'range rover',
+//   {
+//     name: "range rover",
 //     img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ7UDUbCd6nzB5lO33DgkvyXJD4OOoKEsCemg&usqp=CAU",
 //     price: 5328000,
 //     description:
-//       'Редуктивний характер дизайну не містить зайвих деталей, що створює форму, яка демонструє захоплюючий дух сучасності. Це найбажаніший Range Rover серед усіх існуючих.',
+//       "Редуктивний характер дизайну не містить зайвих деталей, що створює форму, яка демонструє захоплюючий дух сучасності. Це найбажаніший Range Rover серед усіх існуючих.",
 //   },
 // ];
 
-// html/
-// <ul class="js-goods"></ul>
+// import cars from "./date/cars.js";
 
-// import cars from "./date/cars.js"
+// const ul = document.querySelector(".js-goods");
+// function elementsCars({ name, img, price, description }) {
+//   const liCar = document.createElement("li");
+//   const heroTitelEl = document.createElement("h1");
+//   const imageEl = document.createElement("img");
+//   const priceEl = document.createElement("p");
+//   const text = document.createElement("p");
 
-// const listEl = document.querySelector('.js-goods');
+//   heroTitelEl.textContent = name;
+//   imageEl.textContent = img;
+//   imageEl.width = 300;
+//   priceEl.textContent = price;
+//   text.textContent = description;
 
-// function createElements ({name, img, price, description}){
-// const oneCar = document.createElement('li');
-// const title = document.createElement('h2');
-// const photo = document.createElement('img');
-// const carPrice = document.createElement('p');
-// const text = document.createElement('p');
-
-// title.textContent = name;
-// photo.src = img;
-// photo.width = 300;
-// carPrice.textContent = price;
-// text.textContent = description;
-
-// oneCar.append(photo, title, carPrice, text);
-// return oneCar;
+//   liCar.append(imageEl, heroTitelEl, priceEl, text);
+//   return liCar;
 // }
 
-// const elements = cars.map(createElements);
-// listEl.append(...elements);
+// const elements = cars.map(elementsCars);
+// ul.append(...elements);
+//==================================\
 
+// 2.2 ПЕРЕПИШІТЬ КОД , ЩОБ ЗНАЧЕННЯ МИ ОТРИМУВАЛИ ПО КЛІЦІ НА КНОПКУ , ПРИ НАЖАТТІ ЯКОГО БУДЕ ВАМ РАХУВАТИ ТА ВИВОДИТИ РЕЗУЛЬТАТ НА ЕКРАН    <button class="btn">Calculate</button>
 
+// 2.3 НАПИШІТЬ  РАНДОМНИЙ КОЛІР ДЛЯ НАШОЇ ФОРМИ КОЛЬКУЛЯТОРА ЗА ДОПОМОГОЮ ІНЛАЙНОВОГО СТИЛЮ
 
-// 2.1НАПИШІТЬ КАЛЬКУЛЯТОР, ЯКИЙ БУДЕ РАХУВАТИ ЦІНУ ШАШЛИКА, В ЗАЛЕЖНОСТІ ВІД ЦІНИ ЗА КІЛОГРАМ І КІЛЬКОСТІ КУПЛЕНОГО ПРОДУКТА , І БУДЕ ВИВОДИТИ РЕЗУЛЬТАТ НА ЕКРАН
+const formEl = document.querySelector(".form");
+const inputPrice = document.querySelector("#price");
+const quantityPrice = document.querySelector("#quantity");
+const totalPrice = document.querySelector(".text-right");
+const spanText = document.querySelector(".amount");
+const div = document.querySelector(".container");
 
-const formEl = document.querySelector('.form');
-const inputPrice = document.querySelector('#price');
-const quantityPrice = document.querySelector('#quantity');
-const totalPrice = document.querySelector('.text-right');
-const spanText = document.querySelector('.amount');
-
-
-formEl.addEventListener('input', calc);
-function calc() {
-    spanText.textContent = quantityPrice.value;
-    const calcTotal = inputPrice.value * quantityPrice.value;
-    console.log(calcTotal);
-    totalPrice.textContent = calcTotal;
+formEl.addEventListener("submit", calc);
+function calc(e) {
+  e.preventDefault();
+  spanText.textContent = quantityPrice.value;
+  const canculator = inputPrice.value * quantityPrice.value;
+  console.log(canculator);
+  totalPrice.textContent = canculator;
 }
-
+formEl.addEventListener("input", newCalc);
+function newCalc(end) {
+  spanText.textContent = quantityPrice.value;
+}
+const randomRgbColor = () => {
+  const r = Math.round(Math.random() * (255 - 1) + 1);
+  const g = Math.round(Math.random() * (255 - 1) + 1);
+  const b = Math.round(Math.random() * (255 - 1) + 1);
+  return `rgb(${r},${g},${b})`;
+};
+div.style.backgroundColor = randomRgbColor();
