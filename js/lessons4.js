8d39ed9383b37c3b492d68aa8657c24d52eda48f
noname068218 <128069790+noname068218@users.noreply.github.com>
2023-08-27 18:28:31 +0200

    ss

--- js/lessons4.js
+++ js/lessons4.js
@@ -61,29 +61,150 @@
 
 // 2.3 НАПИШІТЬ  РАНДОМНИЙ КОЛІР ДЛЯ НАШОЇ ФОРМИ КОЛЬКУЛЯТОРА ЗА ДОПОМОГОЮ ІНЛАЙНОВОГО СТИЛЮ
 
-const formEl = document.querySelector(".form");
-const inputPrice = document.querySelector("#price");
-const quantityPrice = document.querySelector("#quantity");
-const totalPrice = document.querySelector(".text-right");
-const spanText = document.querySelector(".amount");
-const div = document.querySelector(".container");
+// const formEl = document.querySelector(".form");
+// const inputPrice = document.querySelector("#price");
+// const quantityPrice = document.querySelector("#quantity");
+// const totalPrice = document.querySelector(".text-right");
+// const spanText = document.querySelector(".amount");
+// const div = document.querySelector(".container");
 
-formEl.addEventListener("submit", calc);
-function calc(e) {
-  e.preventDefault();
-  spanText.textContent = quantityPrice.value;
-  const canculator = inputPrice.value * quantityPrice.value;
-  console.log(canculator);
-  totalPrice.textContent = canculator;
-}
-formEl.addEventListener("input", newCalc);
-function newCalc(end) {
-  spanText.textContent = quantityPrice.value;
-}
-const randomRgbColor = () => {
-  const r = Math.round(Math.random() * (255 - 1) + 1);
-  const g = Math.round(Math.random() * (255 - 1) + 1);
-  const b = Math.round(Math.random() * (255 - 1) + 1);
-  return `rgb(${r},${g},${b})`;
-};
-div.style.backgroundColor = randomRgbColor();
+// formEl.addEventListener("submit", calc);
+// function calc(e) {
+//   e.preventDefault();
+//   spanText.textContent = quantityPrice.value;
+//   const canculator = inputPrice.value * quantityPrice.value;
+//   console.log(canculator);
+//   totalPrice.textContent = canculator;
+// }
+// formEl.addEventListener("input", newCalc);
+// function newCalc(end) {
+//   spanText.textContent = quantityPrice.value;
+// }
+// const randomRgbColor = () => {
+//   const r = Math.round(Math.random() * (255 - 1) + 1);
+//   const g = Math.round(Math.random() * (255 - 1) + 1);
+//   const b = Math.round(Math.random() * (255 - 1) + 1);
+//   return `rgb(${r},${g},${b})`;
+// };
+// div.style.backgroundColor = randomRgbColor();
+
+// const instruments = [
+//   {
+//     id: 1,
+//     img: 'https://content.rozetka.com.ua/goods/images/big/13664457.jpg',
+//     name: 'Молоток',
+//     price: 150,
+//   },
+//   {
+//     id: 2,
+//     img: 'https://machtz.com.ua/files/resized/products/dsc_2898_new.800x550.jpg',
+//     name: 'Перфоратор',
+//     price: 3000,
+//   },
+//   {
+//     id: 3,
+//     img: 'https://content2.rozetka.com.ua/goods/images/big_tile/232653359.jpg',
+//     name: 'Рівень',
+//     price: 2000,
+//   },
+// ];
+
+// const markup = document.querySelector('.js-list');
+
+// function createMarkup() {
+//   const result = instruments
+//     .map(({ id, img, name, price }) => {
+//       return `    <li data-id = ${id}>
+//       <img src="${img}" alt="${name}">
+//       <h2>${name}</h2>
+//       <p>${price}</p>
+//       <button class = "btn">Купити</button>
+//     </li>`;
+//     })
+//     .join('');
+//   console.log(result);
+//   markup.insertAdjacentHTML('beforeend', result);
+// }
+
+// createMarkup();
+
+// // 3.2 ДОБАВТЕ КНОПКУ, ЧЕРЕЗ ЯКУ МИ КУПЛЯТИМЕМО ТОВАР
+
+// function oneClick(event) {
+//   if (!event.target.classList.contains('btn')) {
+//     return;
+//   }
+//   const btnClick = event.target.closest('li');
+//   console.log(btnClick);
+//   const resultClick = btnClick.dataset.id;
+//   console.log(resultClick);
+
+//   const resultBtn = instruments.find(
+//     instrument => instrument.id == resultClick
+//   );
+//   console.log(resultBtn);
+// }
+// markup.addEventListener('click', oneClick);
+
+// ЗАДАЧА 4
+// ПОТРІБНО СТВОРИТИ Ф-ЦІЮ , ЯКА БУДЕ ШУКАТИ НАРЦИСА.ГОЛОВНА УМОВА НАРЦИСА ЗНАЮТЬ УСІ , НАРЦИС НЕ ЗНАЄ НІКОГО
+// // є елемент масиву , який незнає нікого
+// // якщо не одна така людина , то вертаємо not found
+// // якщо один , то перевірити , чи його знають усі люди, якщо так то вертаємо found
+
+// const people1 = [
+//   {
+//     name: 'Alex',
+//     know: ['Eva', 'Jhon'],
+//   },
+//   {
+//     name: 'Ivan',
+//     know: ['Jhon', 'Alex'],
+//   },
+//   {
+//     name: 'Eva',
+//     know: ['Alex', 'Jhon'],
+//   },
+//   {
+//     name: 'Jhon',
+//     know: [],
+//   },
+// ];
+
+// const people2 = [
+//   {
+//     name: 'Alex',
+//     know: ['Eva', 'Jhon'],
+//   },
+//   {
+//     name: 'Jhon',
+//     know: ['Eva'],
+//   },
+//   {
+//     name: 'Eva',
+//     know: [],
+//   },
+//   {
+//     name: 'Ivan',
+//     know: ['Jhon', 'Alex'],
+//   },
+// ];
+
+// const people3 = [
+//   {
+//     name: 'Alex',
+//     know: ['Eva', 'Jhon'],
+//   },
+//   {
+//     name: 'Jhon',
+//     know: [],
+//   },
+//   {
+//     name: 'Eva',
+//     know: ['Alex', 'Jhon'],
+//   },
+//   {
+//     name: 'Ivan',
+//     know: ['Jhon', 'Alex'],
+//   },
+// ];
